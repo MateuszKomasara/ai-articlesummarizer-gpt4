2c9dcdd42ca153a2e629c4c155e900a16b0d25c7
Mateusz Komasara <mkomasara@hotmail.com>
2023-05-25 15:49:45 -0400

    Changed url to link to this repo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/MateuszKomasara/ai-articlesummarizer-gpt4", "_blank")
           }
           className='black_btn'
         >
