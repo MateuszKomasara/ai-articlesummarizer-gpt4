75d19536373b66e7cd026d3774cc5c131046bcec
Mateusz Komasara <mkomasara@hotmail.com>
2023-05-25 15:36:06 -0400

    First commit of new React article summarizer utilizing Rapid Api's A.I powered article summarizer

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import linkIcon from '../assets/link.svg'
+import loader from '../assets/loader.svg'
+import copy from '../assets/copy.svg'
+import logo from '../assets/logo (1).svg'
+import tick from '../assets/tick.svg'
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    logo,
+    tick
+}
